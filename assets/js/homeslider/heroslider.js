0f965fab921de2c751b7843fb52fda66b625b7e7
it27427 <istiak.tushar.qtec@gmail.com>
2024-12-30 02:02:36 +0600

    heroslider done

---
+++ assets/js/homeslider/heroslider.js
@@ -0,0 +1,21 @@
+var heroSlider = new Swiper('.heroSlider', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: false,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    768: {
+      // slidesPerView: 3
+      slidesPerView: 'auto'
+    }
+  }
+});
