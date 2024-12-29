942c96b762fed0ce4641058ef089bb5a79054e25
it27427 <istiak.tushar.qtec@gmail.com>
2024-12-29 21:09:25 +0600

    back to top added

--- assets/js/back-top/back-top.js
+++ assets/js/back-top/back-top.js
@@ -1,20 +1,10 @@
 up({
-
-    // bottom position
-    bottom: '1rem',
-    // right position
-    right: '1rem',
-    // width
-    width: '2.5rem',
-    // height
-    height: '2.5rem',
-    // background color
-    bg: 'transparent',
-    // custom icon
-    src: 'assets/cursor/chevron-up-solid.svg',
-    // distance from the top to show the back to top button
-    whenShow: 400,
-    // circular button?
-    circle: true
-
-})
+  bottom: '2rem',
+  right: '1.25rem',
+  width: '2.5rem',
+  height: '2.5rem',
+  bg: '#a100ff',
+  src: 'assets/cursor/chevron-up-solid.svg',
+  whenShow: 400,
+  circle: true
+});
