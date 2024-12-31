c648296de252f5e66556ee72017cd5d670d8ad22
it27427 <istiak.tushar.qtec@gmail.com>
2024-12-31 13:20:29 +0600

    all paged updated

---
+++ assets/js/back-top/backtop.js
@@ -0,0 +1,10 @@
+up({
+  bottom: '2rem',
+  right: '1.25rem',
+  width: '2.5rem',
+  height: '2.5rem',
+  bg: '#a100ff',
+  src: '../assets/cursor/chevron-up-solid.svg',
+  whenShow: 400,
+  circle: true
+});
